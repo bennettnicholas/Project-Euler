991929b5c844a2a64281c75ae31d34722cee9d3c
Nicholas Bennett <nicholas.bennett@mines.sdsmt.edu>
2019-07-22 10:20:48 -0500

    Found answer to problem
    Todo, optimize

--- testing python/test.py
+++ testing python/test.py
@@ -5,17 +5,21 @@ isprime = True
 x = 2
 highest = 0
 
-while x < 600851475143:
-
-    for y in primes:
-        if(x%y == 0):
-            isprime = False
-            break
-    if(isprime == True):
-        primes.append(x)
-    isprime = True
+while (x < 600851475143) :
+    if(600851475143 % x == 0):
+        for y in primes:
+            if(x%y == 0):
+                isprime = False
+                break
+        if(isprime == True):
+            primes.append(x)
+            print x
+        isprime = True
+    else:
+        pass
 
     x = x + 1
+
 for z in primes:
     if(600851475143 % z == 0 and z > highest):
         highest = z
